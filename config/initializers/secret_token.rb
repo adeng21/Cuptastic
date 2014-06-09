e0ee3993efa8071566073f8c492460cb12971529
adeng21 <adeng21@gmail.com>
2014-06-09 10:05:50 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cuptastic::Application.config.secret_key_base = '451e7212dea531d59bf120ebb16210da601fee7f035892b48e20268f49f2a096396bce247fba2856f61000fc4605a8cc2120f3f914a732bfd84c572266b30798'
